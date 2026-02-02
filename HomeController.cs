d7daa3fe1d0abf9e3ae303c8ef20fddbc082e80b
Henric Hancus <hanskresna@gmail.com>
2026-02-02 17:48:50 +0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("hai")]
+        public string Hai([FromQuery] string nama)
+        {
+            return "halo, " + nama + "!";
+        }
+    }
+}
